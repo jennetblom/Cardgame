ab533e1b6f63d0531d2115bac2068e71760a2bc5
Jennet Blom <145385182+jennetblom@users.noreply.github.com>
2023-10-19 10:35:01 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,64 +9,18 @@ public class Main {
 
 
         List<Card> cardList=new ArrayList<>();
-        cardList.add(new Card(1,"Hearts"));
-        cardList.add(new Card(2,"Hearts"));
-        cardList.add(new Card(3,"Hearts"));
-        cardList.add(new Card(4,"Hearts"));
-        cardList.add(new Card(5,"Hearts"));
-        cardList.add(new Card(6,"Hearts"));
-        cardList.add(new Card(7,"Hearts"));
-        cardList.add(new Card(8,"Hearts"));
-        cardList.add(new Card(9,"Hearts"));
-        cardList.add(new Card(10,"Hearts"));
-        cardList.add(new Card(11,"Hearts"));
-        cardList.add(new Card(12,"Hearts"));
-        cardList.add(new Card(13,"Hearts"));
-
-        cardList.add(new Card(1,"Diamonds"));
-        cardList.add(new Card(2,"Diamonds"));
-        cardList.add(new Card(3,"Diamonds"));
-        cardList.add(new Card(4,"Diamonds"));
-        cardList.add(new Card(5,"Diamonds"));
-        cardList.add(new Card(6,"Diamonds"));
-        cardList.add(new Card(7,"Diamonds"));
-        cardList.add(new Card(8,"Diamonds"));
-        cardList.add(new Card(9,"Diamonds"));
-        cardList.add(new Card(10,"Diamonds"));
-        cardList.add(new Card(11,"Diamonds"));
-        cardList.add(new Card(12,"Diamonds"));
-        cardList.add(new Card(13,"Diamonds"));
-
-        cardList.add(new Card(1,"Spades"));
-        cardList.add(new Card(2,"Spades"));
-        cardList.add(new Card(3,"Spades"));
-        cardList.add(new Card(4,"Spades"));
-        cardList.add(new Card(5,"Spades"));
-        cardList.add(new Card(6,"Spades"));
-        cardList.add(new Card(7,"Spades"));
-        cardList.add(new Card(8,"Spades"));
-        cardList.add(new Card(9,"Spades"));
-        cardList.add(new Card(10,"Spades"));
-        cardList.add(new Card(11,"Spades"));
-        cardList.add(new Card(12,"Spades"));
-        cardList.add(new Card(13,"Spades"));
 
 
-        cardList.add(new Card(1,"Clubs"));
-        cardList.add(new Card(2,"Clubs"));
-        cardList.add(new Card(3,"Clubs"));
-        cardList.add(new Card(4,"Clubs"));
-        cardList.add(new Card(5,"Clubs"));
-        cardList.add(new Card(6,"Clubs"));
-        cardList.add(new Card(7,"Clubs"));
-        cardList.add(new Card(8,"Clubs"));
-        cardList.add(new Card(9,"Clubs"));
-        cardList.add(new Card(10,"Clubs"));
-        cardList.add(new Card(11,"Clubs"));
-        cardList.add(new Card(12,"Clubs"));
-        cardList.add(new Card(13,"Clubs"));
 
+        for(int number=1; number<=13;number++){
+            cardList.add(new Card(number,"Clubs"));
+            cardList.add(new Card(number,"Diamonds"));
+            cardList.add(new Card(number,"Hearts"));
+            cardList.add(new Card(number,"Spades"));
+        }
         System.out.println(cardList.size());
+
+
 
 
 
